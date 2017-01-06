6462ff7d8296e19f0b17e8c2cd7a5d3f39c785bf
Hubl <hubl@bytter.com>
2017-01-06 15:38:47 +0800

    bug-145-1

--- dbscript/V2.1.0HOTFIX1/ddl/500-ddl-hubl.sql
+++ dbscript/V2.1.0HOTFIX1/ddl/500-ddl-hubl.sql
@@ -3,7 +3,7 @@
 	create procedure change_table_col() BEGIN   
 	IF NOT EXISTS (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA in (select database()) AND table_name='t_scf_request_notice' AND COLUMN_NAME='C_EMAIL')
 	THEN   
-	   ALTER TABLE `t_scf_request_notice` ADD COLUMN `C_EMAIL` `C_EMAIL` VARCHAR(28) NULL COMMENT '邮件';
+	   ALTER TABLE `t_scf_request_notice` ADD COLUMN `C_EMAIL` VARCHAR(28) NULL COMMENT '邮件';
 	END IF;
 	END$$
 	call change_table_col()$$
@@ -13,7 +13,7 @@
 	create procedure change_table_col() BEGIN   
 	IF NOT EXISTS (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA in (select database()) AND table_name='t_scf_request_notice' AND COLUMN_NAME='C_PHONE')
 	THEN   
-	   ALTER TABLE `t_scf_request_notice` ADD COLUMN `C_PHONE` `C_PHONE` VARCHAR(20) NULL COMMENT '联系电话';
+	   ALTER TABLE `t_scf_request_notice` ADD COLUMN `C_PHONE` VARCHAR(20) NULL COMMENT '联系电话';
 	END IF;
 	END$$
 	call change_table_col()$$
@@ -23,7 +23,7 @@
 	create procedure change_table_col() BEGIN   
 	IF NOT EXISTS (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA in (select database()) AND table_name='t_scf_request_opinion' AND COLUMN_NAME='C_LINKNAME')
 	THEN   
-	   ALTER TABLE `t_scf_request_opinion` ADD COLUMN `C_LINKNAME` `C_LINKNAME` VARCHAR(32) NULL COMMENT '联系电话';
+	   ALTER TABLE `t_scf_request_opinion` ADD COLUMN `C_LINKNAME` VARCHAR(32) NULL COMMENT '联系电话';
 	END IF;
 	END$$
 	call change_table_col()$$
@@ -33,7 +33,7 @@
 	create procedure change_table_col() BEGIN   
 	IF NOT EXISTS (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA in (select database()) AND table_name='t_scf_request_opinion' AND COLUMN_NAME='C_EMAIL')
 	THEN   
-	   ALTER TABLE `t_scf_request_opinion` ADD COLUMN `C_EMAIL` `C_EMAIL` VARCHAR(28) NULL COMMENT '邮件';
+	   ALTER TABLE `t_scf_request_opinion` ADD COLUMN `C_EMAIL` VARCHAR(28) NULL COMMENT '邮件';
 	END IF;
 	END$$
 	call change_table_col()$$
@@ -43,7 +43,7 @@
 	create procedure change_table_col() BEGIN   
 	IF NOT EXISTS (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA in (select database()) AND table_name='t_scf_request_opinion' AND COLUMN_NAME='C_PHONE')
 	THEN   
-	   ALTER TABLE `t_scf_request_opinion` ADD COLUMN `C_PHONE` `C_PHONE` VARCHAR(20) NULL COMMENT '联系电话';
+	   ALTER TABLE `t_scf_request_opinion` ADD COLUMN `C_PHONE` VARCHAR(20) NULL COMMENT '联系电话';
 	END IF;
 	END$$
 	call change_table_col()$$
