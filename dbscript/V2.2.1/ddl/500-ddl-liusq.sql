29aa1f57fac619709f8f444e26a6038f94e1fe5f
root <git4bytter@sina.cn>
2017-03-25 14:06:26 +0800

    Change Data Length

--- dbscript/V2.2.1/ddl/500-ddl-liusq.sql
+++ dbscript/V2.2.1/ddl/500-ddl-liusq.sql
@@ -1,9 +1,9 @@
 ##-- 
 	drop PROCEDURE if EXISTS change_table_col$$
 	create procedure change_table_col() BEGIN   
-	IF NOT EXISTS (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA in (select database()) AND table_name='t_temp_cust_enroll' AND COLUMN_NAME='ID')
+	IF EXISTS (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA in (select database()) AND table_name='t_temp_cust_enroll' AND COLUMN_NAME='ID')
 	THEN   
-	   ALTER TABLE `t_temp_cust_enroll` CHANGE `ID` `ID` BIGINT(30);
+	   ALTER TABLE `t_temp_cust_enroll` CHANGE `ID` `ID` BIGINT(18);
 	END IF;
 	END$$
 	call change_table_col()$$
