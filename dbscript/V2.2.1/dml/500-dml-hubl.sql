2e912a9d1f0a708d8e46df2e9f41bceb732b54a3
Hubl <hubl@bytter.com>
2017-03-26 11:33:31 +0800

    菜单设置 sql 优化

--- dbscript/V2.2.1/dml/500-dml-hubl.sql
+++ dbscript/V2.2.1/dml/500-dml-hubl.sql
@@ -1,5 +1,5 @@
 ##--证书管理员默认角色菜单关系数据 提交日期2017/03/24
-DELETE FROM t_cfg_menu_role$$
+DELETE FROM t_cfg_menu_role  where ID in (1,2,3,4,5,6)$$
 INSERT INTO `t_cfg_menu_role` (`id`, `n_rule_id`, `c_rule_name`, `n_menu_id`, `c_menu_name`, `c_status`, `c_shirouser_type`) VALUES (1, 'OPERATOR_ADMIN', NULL, 242, NULL, '1', 'SUPPLIER_USER,SELLER_USER,CORE_USER,FACTOR_USER,PLATFORM_USER')$$
 INSERT INTO `t_cfg_menu_role` (`id`, `n_rule_id`, `c_rule_name`, `n_menu_id`, `c_menu_name`, `c_status`, `c_shirouser_type`) VALUES (6, 'OPERATOR_ADMIN', NULL, 60, NULL, '1', 'SUPPLIER_USER,SELLER_USER,CORE_USER,FACTOR_USER,PLATFORM_USER')$$
 INSERT INTO `t_cfg_menu_role` (`id`, `n_rule_id`, `c_rule_name`, `n_menu_id`, `c_menu_name`, `c_status`, `c_shirouser_type`) VALUES (5, 'OPERATOR_ADMIN', NULL, 64, NULL, '1', 'SUPPLIER_USER,SELLER_USER,CORE_USER,FACTOR_USER,PLATFORM_USER')$$
