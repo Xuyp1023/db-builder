426ca650df660120985dacd29814a43ccdb81bc0
Xuyp <xuyp@bytter.com>
2017-03-28 16:22:16 +0800

    sql结尾

--- dbscript/V2.2.1/dml/500-dml-xuyp.sql
+++ dbscript/V2.2.1/dml/500-dml-xuyp.sql
@@ -14,6 +14,6 @@ INSERT ignore `t_rule_busin_validator` (`ID`, `C_BUSIN_NAME`, `C_VALID_NAME`, `N
 INSERT ignore `t_rule_validator` (`C_SYS`, `C_VALID_NAME`, `C_SHOW_NAME`, `C_NAME`, `C_MUSTITEM`, `C_DATA_TYPE`, `N_DATA_LEN`, `N_DATA_SCALE`, `F_MAX`, `F_MIN`, `C_VALIDATOR`, `C_PATTERN`, `D_REGDATE`, `D_MODIDATE`, `C_DESCRIPTION`, `C_BUSIN_FIELD`, `C_DEPENDS`, `C_MESSAGE`, `C_REF_VALUE`) VALUES ('SCF', 'cre-factorNo', '保理公司编号', 'factorNo', '0', 'LONG', '0', '0', NULL, NULL, 'LONG', NULL, '20160718', NULL, '保理公司编号', NULL, NULL, NULL, NULL)$$
 
 ### bug-459
-update t_rule_validator set c_name='GTEregDate' where c_valid_name='pro-GTEregDate'
+update t_rule_validator set c_name='GTEregDate' where c_valid_name='pro-GTEregDate'$$
 
 
