34824674d98a17770d55b950360131520a9389f5
liuwl <liuwl@bytter.com>
2017-05-03 12:35:28 +0800

    task-193

--- dbscript/V2.3.0/ddl/500-ddl-liuwl.sql
+++ dbscript/V2.3.0/ddl/500-ddl-liuwl.sql
@@ -37,3 +37,77 @@ drop PROCEDURE if EXISTS create_table_seq_record$$
 
 
 
+
+
+##-- task 193
+drop PROCEDURE if EXISTS create_table_domain_attribute$$
+	create procedure create_table_domain_attribute() 
+	BEGIN
+		IF NOT EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA in (select database()) AND table_name='t_sys_domain_attribute')
+		THEN
+			CREATE TABLE `t_sys_domain_attribute` (
+				`ID` BIGINT(20) NOT NULL COMMENT '编号',
+				`C_NAME` VARCHAR(120) NULL DEFAULT NULL COMMENT '参数名',
+				`C_CODE` VARCHAR(120) NULL DEFAULT NULL COMMENT '参数编码',
+				`C_TYPE` CHAR(2) NULL DEFAULT NULL COMMENT '参数类型',
+				`C_DATA_TYPE` CHAR(2) NULL DEFAULT NULL COMMENT '数据类型',
+				`C_OPERORG` VARCHAR(120) NULL DEFAULT NULL COMMENT '机构',
+				`L_CUSTNO` BIGINT(20) NULL DEFAULT NULL COMMENT '公司',
+				`C_CUSTNAME` VARCHAR(120) NULL DEFAULT NULL COMMENT '公司名称',
+				`C_STRING_VALUE` VARCHAR(1024) NULL DEFAULT NULL COMMENT '字符串参数',
+				`N_NUMBER_VALUE` BIGINT(20) NULL DEFAULT NULL COMMENT '数字参数',
+				`F_MONEY_VALUE` DECIMAL(16,2) NULL DEFAULT NULL COMMENT '货币参数',
+				`C_OBJECT_VALUE` LONGTEXT NULL COMMENT '对象参数(JSON)',
+				`T_MODI_TIME` VARCHAR(8) NULL DEFAULT ' ',
+				`D_MODI_DATE` VARCHAR(8) NULL DEFAULT ' ',
+				`C_MODI_OPERNAME` VARCHAR(40) NULL DEFAULT ' ',
+				`L_MODI_OPERID` BIGINT(18) NULL DEFAULT '0',
+				`T_REG_TIME` VARCHAR(8) NULL DEFAULT ' ',
+				`D_REG_DATE` VARCHAR(8) NULL DEFAULT ' ',
+				`C_REG_OPERNAME` VARCHAR(40) NULL DEFAULT ' ',
+				`L_REG_OPERID` BIGINT(18) NULL DEFAULT '0',
+				`N_VERSION` BIGINT(20) NULL DEFAULT '0',
+				`C_BUSIN_STATUS` CHAR(1) NULL DEFAULT NULL COMMENT '状态：0暂存，1已发布，2已撤回，3已删除',
+				`C_LAST_STATUS` CHAR(1) NULL DEFAULT NULL,
+				PRIMARY KEY (`ID`),
+				UNIQUE INDEX `CODE_OPERORG_CUSTNO` (`C_CODE`, `C_OPERORG`, `L_CUSTNO`)
+			)
+			COLLATE='utf8_general_ci'
+			ENGINE=InnoDB
+			;
+		END IF;
+	END$$
+call create_table_domain_attribute()$$
+drop PROCEDURE if EXISTS create_table_domain_attribute$$
+
+drop PROCEDURE if EXISTS create_table_domain_attribute$$
+	create procedure create_table_domain_attribute() 
+	BEGIN
+		IF NOT EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA in (select database()) AND table_name='t_sys_domain_attribute')
+		THEN
+			CREATE TABLE `t_sys_business_type` (
+				`ID` BIGINT(20) NOT NULL COMMENT '编号',
+				`C_CODE` VARCHAR(10) NOT NULL COMMENT '业务类型编码',
+				`C_NAME` VARCHAR(50) NULL DEFAULT NULL COMMENT '业务类型名称',
+				`C_CREDIT_FLAG` CHAR(1) NULL DEFAULT NULL COMMENT '授信标志',
+				`L_MOCK_DATA_ID` BIGINT(20) NULL DEFAULT NULL COMMENT '预览数据编号',
+				`C_COMMENTS` VARCHAR(200) NULL DEFAULT NULL COMMENT '备注',
+				`C_BUSIN_STATUS` CHAR(2) NULL DEFAULT NULL COMMENT '业务状态',
+				`C_DOC_STATUS` CHAR(2) NULL DEFAULT NULL COMMENT '文档状态',
+				`N_VERSION` BIGINT(20) NULL DEFAULT NULL COMMENT '数据版本',
+				PRIMARY KEY (`ID`, `C_CODE`)
+			)
+			COLLATE='utf8_general_ci'
+			ENGINE=InnoDB
+			;
+		END IF;
+	END$$
+call create_table_domain_attribute()$$
+drop PROCEDURE if EXISTS create_table_domain_attribute$$
+
+
+
+
+
+
+
--- dbscript/V2.3.0/dml/500-dml-liuwl.sql
+++ dbscript/V2.3.0/dml/500-dml-liuwl.sql
@@ -4,3 +4,7 @@
 ##
 ##-- 添加系统操作员的默认权限  提交日期2016/11/15
 
+
+DELETE FROM `t_cfg_snogeneral` WHERE ID >=20620 AND ID <= 20629;
+
+INSERT INTO `t_cfg_snogeneral` (`C_OPERTYPE`, `L_LASTNO`, `D_LASTDATE`, `C_SYSNO`, `C_TYPE`, `C_MSG`, `L_DATA_LEN`, `ID`) VALUES ('DomainAttribute.id', '1', NULL, 'SYS', '0', NULL, '10', '20601')$$
