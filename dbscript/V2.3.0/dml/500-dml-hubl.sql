ed498c88498a78f5885f28263cf98a31b9bc0854
Hubl <hubl@bytter.com>
2017-05-12 17:03:31 +0800

    task-202-1

--- dbscript/V2.3.0/dml/500-dml-hubl.sql
+++ dbscript/V2.3.0/dml/500-dml-hubl.sql
@@ -3,7 +3,7 @@
 ##-- V2.1.0
 ##
 ##-- 添加系统操作员的默认权限  提交日期20170512
-delete from t_cfg_snogeneral where id in (50014,50015,50017);
-INSERT INTO `t_cfg_snogeneral` (`C_OPERTYPE`, `L_LASTNO`, `D_LASTDATE`, `C_SYSNO`, `C_TYPE`, `C_MSG`, `L_DATA_LEN`, `ID`) VALUES ('CommissionDailyStatement.id', 0, NULL, 'SCF', '0', NULL, 10, 50014);
-INSERT INTO `t_cfg_snogeneral` (`C_OPERTYPE`, `L_LASTNO`, `D_LASTDATE`, `C_SYSNO`, `C_TYPE`, `C_MSG`, `L_DATA_LEN`, `ID`) VALUES ('CommissionMonthlyStatementRecord.id', 0, NULL, 'SCF', '0', NULL, 10, 50015);
-INSERT INTO `t_cfg_snogeneral` (`C_OPERTYPE`, `L_LASTNO`, `D_LASTDATE`, `C_SYSNO`, `C_TYPE`, `C_MSG`, `L_DATA_LEN`, `ID`) VALUES ('CommissionMonthlyStatement.id', 0, NULL, 'SCF', '0', NULL, 10, 50017);
+delete from t_cfg_snogeneral where id in (50014,50015,50017)$$
+INSERT INTO `t_cfg_snogeneral` (`C_OPERTYPE`, `L_LASTNO`, `D_LASTDATE`, `C_SYSNO`, `C_TYPE`, `C_MSG`, `L_DATA_LEN`, `ID`) VALUES ('CommissionDailyStatement.id', 0, NULL, 'SCF', '0', NULL, 10, 50014)$$
+INSERT INTO `t_cfg_snogeneral` (`C_OPERTYPE`, `L_LASTNO`, `D_LASTDATE`, `C_SYSNO`, `C_TYPE`, `C_MSG`, `L_DATA_LEN`, `ID`) VALUES ('CommissionMonthlyStatementRecord.id', 0, NULL, 'SCF', '0', NULL, 10, 50015)$$
+INSERT INTO `t_cfg_snogeneral` (`C_OPERTYPE`, `L_LASTNO`, `D_LASTDATE`, `C_SYSNO`, `C_TYPE`, `C_MSG`, `L_DATA_LEN`, `ID`) VALUES ('CommissionMonthlyStatement.id', 0, NULL, 'SCF', '0', NULL, 10, 50017)$$
